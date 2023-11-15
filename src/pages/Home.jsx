0a8e3cce5165c99ddbbe01b3d0e2435de76582d1
atikurahad <atikurahad@gmail.com>
2023-11-15 23:01:19 +0600

    home complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,34 +3,53 @@ import photo1 from "../assets/images/Rectangle1.png";
 import photo2 from "../assets/images/Rectangle2.png";
 import photo3 from "../assets/images/Rectangle3.png";
 import photo4 from "../assets/images/Rectangle4.png";
+import google from "../assets/images/logos_google.png";
+import monday from "../assets/images/logos_monday.png";
+import notion from "../assets/images/Notion.png";
+import slack from "../assets/images/Slack.png";
+import trello from "../assets/images/Trello-logo-blue 1.png";
 
 const Home = () => {
   return (
-    <div className="bg-[#D7F5DC] h-[800px] w-[1440px]">
-      <div className="grid grid-cols-2 mx-6">
-        <section>
-          <h1 className="text-6xl font-bold">
-            Increase Your Coustomers Loyalty and Satisfaction
-          </h1>
-          <p>we help businesses like yours earn more coustomers,</p>
-          <p>standout from competitors, make more money</p>
-          <button className="btn  bg-[#20B15A]">Get Started</button>
-        </section>
+    <>
+      <div className="bg-[#D7F5DC]">
+        <div className="grid grid-cols-2 mx-6 pb-12 pt-6">
+          <section>
+            <h1 className="text-[50px] font-bold mx-6 p-4">
+              Increase Your Coustomers Loyalty and Satisfaction
+            </h1>
+            <p className="text-[20px] font-bold mx-6 p-4">
+              we help businesses like yours earn more coustomers,standout from
+              competitors, make more money
+            </p>
+            <button className="btn  bg-[#20B15A] ml-10 p-4 ">
+              Get Started
+            </button>
+          </section>
 
-        <section className="flex  flex-col p-4 gap-5">
-          <div className=" items-center justify-between">
-            <img src={photo1} />
-            <img src={photo2} />
-        
-          </div>
-          <div className="flex items-center justify-between">
-           
-            <img src={photo3} />
-            <img src={photo4} />
-          </div>
+          <section className="flex  flex-col  gap-5">
+            <div className="flex gap-4">
+              <img className="w-full" src={photo1} />
+              <img className="w-full" src={photo2} />
+            </div>
+            <div className="flex gap-0">
+              <img className="w-full" src={photo3} />
+              <img className="w-full" src={photo4} />
+            </div>
+          </section>
+        </div>
+      </div>
+
+      <div className="py-8">
+        <section className="flex flex-row items-center justify-center gap-6">
+          <img src={google} />
+          <img src={trello} />
+          <img src={monday} />
+          <img src={notion} />
+          <img src={slack} />
         </section>
       </div>
-    </div>
+    </>
   );
 };
 
